5e38437717a00e275b6c5a365c31f0535e841b66
Mariah Alderson <mariah@Mariahs-MBP.fanshawec.ca>
2019-02-11 15:34:03 -0500

    tile styling

--- js/components/layout/ContentComponent.js
+++ js/components/layout/ContentComponent.js
@@ -15,27 +15,39 @@ export default {
         </section>
 
         <section id="contentCon">
-        <section id="mainDesc">
-            <h2>Ontario Summer</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </section>
-        <section id="videoSect"></section>
-          <h2 class="hidden">Ontario Summer Video</h2>
-        <section id="picSect">
-          <h2 class="hidden">Picture Gallery</h2>
-            <section class="box">
-              <h2 class="hidden">Picture</h2>
-            </section>
-            <section class="box">
-              <h2 class="hidden">Picture</h2>
-            </section>
-            <section class="box">
-              <h2 class="hidden">Picture</h2>
-            </section>
-            <section class="box">
-              <h2 class="hidden">Picture</h2>
-            </section>
-        </section>
+
+          <section id="mainDesc">
+              <h2>Ontario Summer</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </section>
+
+          <section id="videoSect"></section>
+            <h2 class="hidden">Ontario Summer Video</h2>
+
+          <section id="picSect">
+            <h2 class="hidden">Picture Gallery</h2>
+
+              <section class="box">
+                <h2 class="hidden">Picture</h2>
+                  
+              </section>
+
+              <section class="box">
+                <h2 class="hidden">Picture</h2>
+
+              </section>
+
+              <section class="box">
+                <h2 class="hidden">Picture</h2>
+
+              </section>
+
+              <section class="box">
+                <h2 class="hidden">Picture</h2>
+
+              </section>
+          </section>
+
         </section>
       </section>
   `,
