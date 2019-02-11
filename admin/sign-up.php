e74560b5ce4b78cc123615a22fa3c77dc9598631
Dani Dantas <dani@DaniVonRohr.fanshawec.ca>
2019-02-11 13:19:35 -0500

    Fetch POST working

--- admin/sign-up.php
+++ admin/sign-up.php
@@ -5,22 +5,23 @@ require_once 'scripts/config.php';
 
 // confirm_logged_in();
 
-if (isset($_POST['submit'])) {
-
-  // Validation.1
+    // var_dump($_POST);die;
     $firstname = htmlspecialchars(trim($_POST['first-name']));
     $lastname = htmlspecialchars(trim($_POST['last-name']));
     $email = htmlspecialchars(trim($_POST['email']));
     $country_id = htmlspecialchars(trim($_POST['countries']));
-
+ 
     // Validation.2
     if (empty($firstname) || empty($lastname) || empty($email) | empty($country_id)) {
-        $message = "Please fill in the required fields";
+        $message = "Subscription Failed";
     } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
         // validate email
         $message = 'Please use a valid email';
     } else {
+        
         $result = createSubscriber($firstname, $lastname, $email, $country_id);
         $message = $result;
     }
-}
+
+    echo json_encode($result);
+
