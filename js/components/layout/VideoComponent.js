bfa48dc169ad95a9b77c0a9cd719eff5d7388807
Dani Dantas <dani@DaniVonRohr.fanshawec.ca>
2019-02-11 20:00:16 -0500

    cleaning up in video component

--- js/components/layout/VideoComponent.js
+++ js/components/layout/VideoComponent.js
@@ -3,27 +3,10 @@ export default {
   template: `
   <section id="videoSect">
   <div class="fullscreen-vid-wrap">
-  <video id="vid" autoplay muted loop>
+  <video id="vid" autoplay muted loop controls>
 
   </video>
-  <div class="controls">
-          <ul>
-            <li class="rewindToStart">
-                  <i @click="rWindVid" class="fas fa-fast-backward"></i>
-                </li>
-            <li class="play-pause">
-                  <i @click="togglePlay" class="fas fa-pause"></i>
-                </li>
-            <li><input type="range" id="clipBar" value="0"></li>
-            <li>
-                  <p id="clipTiming">0:00 / 0:00</p>
-                </li>
-            <li id="muteBtn"><i @click="volOn" class="fas fa-volume-up"></i></li>
-            <li><i @click="volOff" class="fas fa-volume-mute"></i></li>
-            
-          </ul>
-
-        </div>
+  
   </div>
   </section>
   `,
@@ -56,10 +39,6 @@ export default {
     },
 
 
-    // UPDATE VOLUME ON volume bar
-    changeVolume() {
-      vid.volume = volumeBar.value;
-    },
     videoSource(element, src, type) {
       var source = document.createElement("source");
 
